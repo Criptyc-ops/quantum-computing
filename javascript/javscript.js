a00d9e86011740568be83ca5108ad624da15311d
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-03-28 17:49:50 +0000

    start

---
+++ javascript/javscript.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
