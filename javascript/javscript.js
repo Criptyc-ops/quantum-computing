669257476e5dfb52731fb9e1518a61e4d5b8dfb1
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-04-02 14:34:19 +0000

    im trying dawg

--- javascript/javscript.js
+++ javascript/javscript.js
@@ -1,4 +1,32 @@
-var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
+var toggleButton = $('<button id="toggle-menu-btn">Toggle Menu</button>');
+$('body').prepend(toggleButton);
 
+// Create and prepend the hamburger button
+var toggleButton = $(`
+    <button id="toggle-menu-btn">
+        <div></div>
+        <div></div>
+        <div></div>
+    </button>
+`);
+$('body').prepend(toggleButton);
+
+// Create the hidden menu
+var hiddenMenu = $(`
+    <div id="hidden-menu">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About Quantum Computing History</a></li>
+            <li><a href="works.html">How It All Works Together</a></li>
+            <li><a href="why.html">Why Quantum Computing Is Important</a></li>
+            <li><a href="support.html">Support</a></li>
+        </ul>
+    </div>
+`);
+$('body').prepend(hiddenMenu);
+
+// Toggle the visibility of the hidden menu and update the hamburger button
+$('#toggle-menu-btn').on('click', function () {
+    $('#hidden-menu').toggle();
+    $(this).toggleClass('open'); // Add/remove the 'open' class for styling
+});
